f91b86cfd72afeb6d0dc34835d4976f3748b7438
Filipe de Oliveira Ataíde <filipe.ataide@academico.ifrn.edu.br>
2022-02-24 13:31:27 -0300

    Reorganizando css e localizacao dos arquivos.

---
+++ figura-interativa/figura-interativa.js
@@ -0,0 +1,48 @@
+$('g').not('#linha').hide();
+$('#imobilidade').hide();
+
+$('text').click(function () {
+  var target = $('#' + $(this).data('target'));
+  target.show();
+  if (target.data('target') == 'imobilidade') {
+    $('#imobilidade').show();
+  }
+})
+
+var min_width = $('svg').width();
+var max_width = min_width * 1.5;
+
+$('.font-plus').click(function () {
+  var w = $('svg').width();
+  if (w < max_width) {
+    w += (min_width * 0.1);
+    $('svg').css('width', w + 'px');
+  }
+})
+
+$('.font-minus').click(function () {
+  var w = $('svg').width();
+  if (w > min_width) {
+    w -= (min_width * 0.1);
+    $('svg').css('width', w + 'px');
+  }
+})
+
+$('.form-check-input').change(function () {
+  if ($(this).is(':checked')) {
+    $('body').css('background-color', '#000');
+    $('h3, i').css('color', '#fff');
+    $('text').css('fill', '#fff');
+    $('path').css('fill', '#B9B9B9');
+    $('path, line').css('stroke', '#B9B9B9');
+    $('.alert').addClass('alert-dark');
+  }
+  else {
+    $('body').css('background-color', '#fff');
+    $('h3, i').css('color', '#000');
+    $('text').css('fill', '#000');
+    $('path').css('fill', '#0044F3');
+    $('path, line').css('stroke', '#0044F3');
+    $('.alert').removeClass('alert-dark');
+  }
+})
--- unidades/unidades.js
+++ unidades/unidades.js
@@ -18,25 +18,24 @@ $('.form-check-input').change(function () {
     if ($(this).is(':checked')) {
         $('body').css('background-color', '#1C1C1C');
         $('.navbar').css('background-color', '#B9B9B9');
-        $('.navbar').removeClass('navbar-dark');
         $('h5, p').css('color', '#fff');
-        $('#titulo').css('fill', '#fff');
-        botoes.find('rect').css('fill', '#B9B9B9');
-        $('#fase rect').css('fill', '#B9B9B9');
-        $('#titulo rect').css('fill', '#838383');
-        $('#titulo rect').css('stroke', 'white');
         $('path').css('fill', 'white');
+        
+        $('.navbar').removeClass('navbar-dark');
+        $('#titulo').addClass('titulo-dark');
+        botoes.find('rect').addClass('botao-dark');
+        $('#fase rect').addClass('botao-dark');
     }
     else {
         $('body').css('background-color', 'white');
         $('.navbar').css('background-color', '#5A729A');
-        $('.navbar').addClass('navbar-dark');
         $('h5, p').css('color', '#000');
-        $('#titulo').css('fill', '#000');
-        botoes.find('rect').css('fill', '#96D2E7');
-        $('#fase rect').css('fill', '#FF8D3E');
-        $('#titulo rect').css('fill', '#5a729a');
         $('path').css('fill', 'black');
+
+        $('.navbar').addClass('navbar-dark');
+        $('#titulo').removeClass('titulo-dark');
+        botoes.find('rect').removeClass('botao-dark');
+        $('#fase rect').removeClass('botao-dark');
     }
 })
 var min_width = $('svg').width();

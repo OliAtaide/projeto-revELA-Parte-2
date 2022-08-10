21b23ea799e9ee80b84fe5f9af6f3708a9db431b
Filipe de Oliveira de Ataíde <filipe.ataide@lais.huol.ufrn.br>
2022-08-10 10:45:21 -0300

    q2u2 - Estilizando botoes e adicionando nos campos.

--- q2u2/script.js
+++ q2u2/script.js
@@ -1,7 +1,34 @@
 // $('button').button().draggable({cancel:false});
 
-$('i').click(function () {
+var valor;
+
+$('.field').on('click', 'i', function () {
+    console.log($(this));
+
     var target = $(this).data('target');
-    console.log($('[data-button=' + target + ']'));
+    $('#' + target).removeClass('hidden');
     $('[data-button=' + target + ']').remove();
+
+    console.log($('#' + target));
+})
+
+$('button').focus(function () {
+    valor = $(this).attr('id');
+    $(this).addClass('ativo');
+
+})
+
+$('.field').click(function () {
+    var color = $('#' + valor).attr('color');
+
+    if($('.ativo').length != 0){
+        $(this).append(
+            '<h5 class="added" data-button="' + valor + '"><span class="badge rounded-pill bg' + color + '">' +
+                $('#' + valor).text() +
+            '<i data-target="' + valor + '" class="bi bi-x-circle-fill"></i></span></h5>'
+        )
+    }
+
+    $('#' + valor).addClass('hidden');
+    $('.ativo').removeClass('ativo');
 })

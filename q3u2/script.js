6d3851979e1627d997cc85efd4405828b04448a8
Filipe de Oliveira de Ataíde <filipe.ataide@lais.huol.ufrn.br>
2022-08-11 09:57:41 -0300

    q1u4

--- q3u2/script.js
+++ q3u2/script.js
@@ -1,1 +1,36 @@
-$('button').button().draggable({cancel:false});
+$('button').button().draggable({
+    cancel:false,
+    stop:function () {
+        var button = $(this);
+        $('.slot').each(function (index, value) {
+            isOverlapping(button, $(value));
+        })
+    }
+});
+
+// console.log($('.slot').offset());
+// console.log($('.slot').offset().top + $('.slot').height());
+
+function isOverlapping(button, slot){
+    var slt_top = slot.offset().top;
+    var slt_lft = slot.offset().left;
+    var slt_btm = slt_top + slot.height();
+    var slt_rgt = slt_lft + slot.width();
+
+    var btn_vrt = button.offset().top + (button.height() / 2);
+    var btn_hzn = button.offset().left + (button.width() / 2);
+
+    var filled = slot.attr('filled');
+    var isFilled = filled == "false";
+
+    var vertical = btn_vrt > slt_top && btn_vrt < slt_btm;
+    var horizontal = btn_hzn > slt_lft && btn_hzn < slt_rgt;
+
+    console.log(isFilled)
+
+    if(vertical && horizontal && isFilled){
+        button.css('visibility', 'hidden');
+        slot.html('<span>' + button.text() + '</span>');
+        slot.attr('filled', 'true');
+    }
+}
